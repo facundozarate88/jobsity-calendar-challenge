e00268e25145e6956f09d404a292d9f8da495348
Facundo Zarate <facundo.zarate@despegar.com>
2019-12-02 20:09:06 -0300

    Reminder component & interface added

---
+++ src/app/reminder.ts
@@ -0,0 +1,8 @@
+export class Reminder {
+  id: number;
+  date: any;
+  time: string;
+  city: string;
+  color: string;
+  text: string[30];
+}
---
+++ src/app/reminder/reminder.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Reminder } from '../reminder';
+import { EditReminderDialogComponent } from '../edit-reminder-dialog/edit-reminder-dialog.component';
+
+import * as Moment from 'moment';
+import { extendMoment } from 'moment-range';
+import { ReminderService } from '../services/reminder.service';
+
+const moment = extendMoment(Moment);
+
+@Component({
+  selector: 'app-reminder',
+  templateUrl: './reminder.component.html',
+  styleUrls: ['./reminder.component.scss']
+})
+export class ReminderComponent implements OnInit {
+
+  @Input() reminder: Reminder;
+
+  constructor(
+    public dialog: MatDialog,
+    private reminderService: ReminderService,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  editReminder() {
+    const dialogRef = this.dialog.open(EditReminderDialogComponent, {
+      width: '300px',
+      data: this.reminder,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        result.date = moment(result.date).format('YYYY-MM-DD');
+        this.reminderService.updateReminder(result as Reminder);
+      }
+    });
+  }
+
+}
