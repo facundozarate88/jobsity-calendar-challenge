62c94cfd98706b9d21d36efb54026d4d31709b55
Facundo Zarate <facundo.zarate@despegar.com>
2019-12-02 20:16:34 -0300

    WIP environment added for openWeather api call

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'e611fe47fd5aafbae0723ca33910cac5',
+  apiUrl: 'http://api.openweathermap.org/data/2.5'
 };
 
 /*
