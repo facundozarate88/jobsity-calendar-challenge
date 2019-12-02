052881169101a945d0bc7d4eb2554f3484237a80
Facundo Zarate <facundo.zarate@despegar.com>
2019-12-02 20:10:29 -0300

    Edit reminder popup added

---
+++ src/app/edit-reminder-dialog/edit-reminder-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { DateService } from '../services/date.service';
+import { Reminder } from '../reminder';
+
+@Component({
+  selector: 'app-edit-reminder-dialog',
+  templateUrl: './edit-reminder-dialog.component.html',
+  styleUrls: ['./edit-reminder-dialog.component.scss']
+})
+export class EditReminderDialogComponent implements OnInit {
+
+  constructor(
+    public dateService: DateService,
+    public dialogRef: MatDialogRef<EditReminderDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Reminder,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+
+  ngOnInit() {
+  }
+
+}
